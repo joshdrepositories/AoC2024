fe34b8b5646c8514039ed55bb48b4781725ab51f
Josh Daniel <jd8287dan@proton.me>
2024-12-01 21:29:43 -0500

    sort columns numerically

--- Day_1/distance.sh
+++ Day_1/distance.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-grep -o -- '[0-9].* ' input.txt | sed 's/   //' > column1.txt
-grep -o -- ' .[0-9]*$' input.txt | sed 's/  //' > column2.txt
+
+#grep each column, remove whitespace, sort numerically
+grep -o -- '[0-9].* ' input.txt | sed 's/   //' | sort -n > column1.txt
+grep -o -- ' .[0-9]*$' input.txt | sed 's/  //' | sort -n > column2.txt
