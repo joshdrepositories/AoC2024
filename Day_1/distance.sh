a85c4cd283e2ad22c08e59ae814b93bd75a89e29
Josh Daniel <jd8287dan@proton.me>
2024-12-02 15:42:52 -0500

    using aliases was a dumb idea; for loop to iterate through each line

--- Day_1/distance.sh
+++ Day_1/distance.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-line_number=1
-
-shopt -s expand_aliases
 
 #grep each column, remove whitespace, sort numerically
 grep -o -- '[0-9].* ' input.txt | sed 's/   //' | sort -n > column1.txt
 grep -o -- ' .[0-9]*$' input.txt | sed 's/  //' | sort -n > column2.txt
 
-alias column1_number='sed "$line_number!d" column1.txt'
-alias column2_number='sed "$line_number!d" column2.txt'
+for line_number in {1..1000}; do
+    value1=$(sed "$line_number!d" column1.txt)
+    value2=$(sed "$line_number!d" column2.txt)
 
-value1=$(column1_number)
-value2=$(column2_number)
-
-#if value
-
-awk "BEGIN { diff = $value1 - $value2; print diff }"
+    if [ $value1 -gt $value2 ]; then
+        awk "BEGIN { diff = $value1 - $value2; print diff }"
+    else
+        awk "BEGIN { diff = $value2 - $value1; print diff }"
+    fi
+done
