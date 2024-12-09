e91ad083d5ffcc759471be16d566d61e8fb1c4b2
Josh Daniel <jd8287dan@proton.me>
2024-12-09 16:04:29 -0500

    needed extended regular expressions to account for different length integers

--- Day_3/instruction.sh
+++ Day_3/instruction.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-grep -o -- "mul(.[0-9].,.[0-9].)" input.txt | sed 's/mul//' | sed 's/,/*/' | sed 's/((/(/' | sed 's/))/)/' > uncorrupted.txt
+grep -Eo -- 'mul\([0-9]+,[0-9]+\)' input.txt | sed 's/mul//' | sed 's/,/*/' | sed 's/((/(/' | sed 's/))/)/' > uncorrupted.txt
 
-for line_number in {1..553}; do
+total_lines=$(wc -l < uncorrupted.txt)
+
+for (( line_number=1; line_number<=total_lines; line_number++ )); do
     line_content=$(sed "$line_number!d" uncorrupted.txt)
     echo $line_content
 
